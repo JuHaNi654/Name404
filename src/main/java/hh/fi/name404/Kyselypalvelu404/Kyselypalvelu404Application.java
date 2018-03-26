f3733e8fe7eeac4924062e2f759a0ae657c91105
unknown <a1800312@myy.haaga-helia.fi>
2018-03-26 13:53:43 +0300

    first commit

---
+++ src/main/java/hh/fi/name404/Kyselypalvelu404/Kyselypalvelu404Application.java
@@ -0,0 +1,12 @@
+package hh.fi.name404.Kyselypalvelu404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kyselypalvelu404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kyselypalvelu404Application.class, args);
+	}
+}
---
+++ src/test/java/hh/fi/name404/Kyselypalvelu404/Kyselypalvelu404ApplicationTests.java
@@ -0,0 +1,16 @@
+package hh.fi.name404.Kyselypalvelu404;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kyselypalvelu404ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
