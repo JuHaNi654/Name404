0b2f04c5eeaed261182e01b2240359603859fc9b
unknown <a1800312@myy.haaga-helia.fi>
2018-03-29 14:19:49 +0300

    blaablaa

--- src/main/java/hh/fi/name404/Kyselypalvelu404/domain/KysymysRepository.java
+++ src/main/java/hh/fi/name404/Kyselypalvelu404/domain/KysymysRepository.java
@@ -1,9 +1,9 @@
 package hh.fi.name404.Kyselypalvelu404.domain;
 
-
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface KysymysRepository extends CrudRepository<Kysymys, Long> {
+
 	Kysymys findOne(Long id);
+	
 }
