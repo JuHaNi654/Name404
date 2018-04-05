f97d42643f7adf7bae8e7994b17fabb856a3931e
Max <max.laihonen@gmail.com>
2018-04-05 13:52:54 +0300

    Jaaha

--- src/main/java/hh/fi/name404/Kyselypalvelu404/Kyselypalvelu404Application.java
+++ src/main/java/hh/fi/name404/Kyselypalvelu404/Kyselypalvelu404Application.java
@@ -2,9 +2,27 @@ package hh.fi.name404.Kyselypalvelu404;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/*
+@SpringBootApplication
+public class Kyselypalvelu404Application extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder	application) {
+		return application.sources(Kyselypalvelu404Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kyselypalvelu404Application.class, args);
+	}
+
+}
+*/
 
 @SpringBootApplication
-public class Kyselypalvelu404Application {
+public class Kyselypalvelu404Application  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Kyselypalvelu404Application.class, args);
