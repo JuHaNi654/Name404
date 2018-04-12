63867c04080ed7737d0f2edb3c1927295b0690e3
JuHaNi654 <j.turpeinen20@gmail.com>
2018-04-12 13:21:59 +0300

    Added KyselyController fixed

--- src/main/java/hh/fi/name404/Kyselypalvelu404/web/KyselyController.java
+++ src/main/java/hh/fi/name404/Kyselypalvelu404/web/KyselyController.java
@@ -1,5 +1,30 @@
 package hh.fi.name404.Kyselypalvelu404.web;
 
-public class KyselyController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import hh.fi.name404.Kyselypalvelu404.domain.Kysely;
+import hh.fi.name404.Kyselypalvelu404.domain.KyselyRepository;
+import hh.fi.name404.Kyselypalvelu404.domain.KysymysRepository;
+import hh.fi.name404.Kyselypalvelu404.domain.RyhmaRepository;
+
+@Controller
+public class KyselyController {
+	@Autowired KyselyRepository krepository;
+	@Autowired KysymysRepository kyrepository;
+	
+	@GetMapping(value="/kyselyt")
+	public @ResponseBody List<Kysely> kyselyListRest() {
+		return (List<Kysely>) krepository.findAll();
+	}
+	@GetMapping(value="/kyselyt/{id}")
+	public @ResponseBody  Kysely findKyselyRest(@PathVariable("id") Long kyselyid) {
+		return krepository.findOne(kyselyid);
+	}
+	
 }
