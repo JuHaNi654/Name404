3160cfb9e9e2468f67dff0257d0edc52c971f828
Max <max.laihonen@gmail.com>
2018-04-19 13:02:11 +0300

    Kysymys not null

--- src/main/java/hh/fi/name404/Kyselypalvelu404/domain/Vastaus.java
+++ src/main/java/hh/fi/name404/Kyselypalvelu404/domain/Vastaus.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -20,6 +21,7 @@ public class Vastaus {
 	@ManyToOne
 	@JsonBackReference
 	@JoinColumn(name="kysymysid")
+	@NotNull
 	private Kysymys kysymys;
 
 	public Vastaus() {
